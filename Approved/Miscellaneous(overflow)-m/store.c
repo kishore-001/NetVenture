81d374113d9398be0235a72c5d19d7cddb804b86
kishore-001 <kishoreedu07@gmail.com>
2025-02-13 19:48:13 +0530

    changes maded

--- Approved/Miscellaneous(overflow)-m/store.c
+++ Approved/Miscellaneous(overflow)-m/store.c
@@ -8,11 +8,11 @@ int main()
     int account_balance = 1100;
     while(con == 0){
         
-        printf("Welcome to the flag exchange\n");
-        printf("We sell flags\n");
+        printf("Welcome to the Candy exchange\n");
+        printf("We sell Candys\n");
 
         printf("\n1. Check Account Balance\n");
-        printf("\n2. Buy Flags\n");
+        printf("\n2. Buy Candys\n");
         printf("\n3. Exit\n");
         int menu;
         printf("\n Enter a menu selection\n");
@@ -23,20 +23,20 @@ int main()
         }
         else if(menu == 2){
             printf("Currently for sale\n");
-            printf("1. Defintely not the flag Flag\n");
-            printf("2. 1337 Flag\n");
+            printf("1. Defintely not the Candy Candy\n");
+            printf("2. 1337 Candy\n");
             int auction_choice;
             fflush(stdin);
             scanf("%d", &auction_choice);
             if(auction_choice == 1){
-                printf("These knockoff Flags cost 900 each, enter desired quantity\n");
+                printf("These knockoff Candys cost 900 each, enter desired quantity\n");
                 
-                int number_flags = 0;
+                int number_Candys = 0;
                 fflush(stdin);
-                scanf("%d", &number_flags);
-                if(number_flags > 0){
+                scanf("%d", &number_Candys);
+                if(number_Candys > 0){
                     int total_cost = 0;
-                    total_cost = 900*number_flags;
+                    total_cost = 900*number_Candys;
                     printf("\nThe final cost is: %d\n", total_cost);
                     if(total_cost <= account_balance){
                         account_balance = account_balance - total_cost;
@@ -54,7 +54,7 @@ int main()
                 
             }
             else if(auction_choice == 2){
-                printf("1337 flags cost 100000 dollars, and we only have 1 in stock\n");
+                printf("1337 Candys cost 100000 dollars, and we only have 1 in stock\n");
                 printf("Enter 1 to buy one");
                 int bid = 0;
                 fflush(stdin);
@@ -66,12 +66,12 @@ int main()
                         FILE *f = fopen("flag.txt", "r");
                         if(f == NULL){
 
-                            printf("flag not found: please run this on the server\n");
+                            printf("Candy not found: please run this on the server\n");
                             exit(0);
                         }
                         char buf[64];
                         fgets(buf, 63, f);
-                        printf("YOUR FLAG IS: %s\n", buf);
+                        printf("YOUR Candy IS: %s\n", buf);
                         }
                     
                     else{
